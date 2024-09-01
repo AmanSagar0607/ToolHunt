091d644ba96b08a7b4c9b3e462139b5795c209ac
Aman Sagar <62849475+AmanSagar0607@users.noreply.github.com>
2024-09-02 03:47:15 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,33 +5,29 @@ const Footer = ({ isDarkMode }) => {
   return (
     <footer
       className={`footer flex sm:flex-row flex-col items-center justify-between w-full mt-6 sm:mt-20 sm:mb-8 mb-4 ${
-        isDarkMode ? 'bg-[#373737] text-white' : 'bg-gray-400 text-black'
+        isDarkMode ? 'bg-[#373737] text-white' : ' text-black'
       } p-4`}
     >
-      <div className="flex flex-col sm:flex-row gap-2 sm:gap-3 sm:pl-10 pl-0 pt-3 sm:pt-2 sm:mb-2 mb-5 text-center">
-        <span className="block text-xl sm:text-xl font-normal">
+      <div className="flex flex-col sm:flex-row gap-2 sm:gap-3">
+        <span className="block text:xl sm:text-xl font-normal">
           ToolHunt |{' '}
           <span className="sm:inline text-sm font-normal">
-            Made with ❤️ in India
+            Developed by{' '}
+            <a
+              href="https://linktr.ee/amansagar0607"
+              className={`hover:underline ${
+                isDarkMode ? 'text-gray-400' : 'text-gray-600'
+              }`}
+            >
+              Aman Sagar
+            </a>
           </span>
-        </span>
-        <span className="block sm:inline text-sm font-normal">
-          Developed by{' '}
-          <a
-            href="https://linktr.ee/amansagar0607"
-            className={`hover:underline ${
-              isDarkMode ? 'text-gray-400' : 'text-gray-600'
-            }`}
-          >
-            Aman Sagar
-          </a>
         </span>
       </div>
 
       <div className="social-links flex gap-5">
         <a
           href="https://twitter.com/AmanSagar0607"
-          target="_blank"
           rel="noopener noreferrer"
           className="text-lg"
         >
